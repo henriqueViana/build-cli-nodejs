0e93f1ec2ed2c19aa157aa25407f33419630f9d0
henrique.viana <henrique.viana@CIPA01.LOCAL>
2020-02-13 17:20:25 -0300

    config initialize

--- src/index.js
+++ src/index.js
@@ -3,3 +3,17 @@ const clear = require('clear')
 const figlet = require('figlet')
 
 const files = require('./helpers/files')
+
+clear()
+
+console.log(
+  chalk.yellow(
+    figlet.textSync('NodeJS CLI', { horizontalLayout: 'full'})
+  )
+)
+
+if (files.directoryExists('.git')) {
+  console.log(
+    chalk.red('A Git repository !!!')
+  )
+}
